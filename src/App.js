a1dca4ee119c4733edbe1e4b9ab461b2c9109ee0
Valentina Otočan <valentinaotocan@gmail.com>
2022-09-30 10:30:10 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -2,26 +2,21 @@ import React from 'react';
 import "./style.css";
 import Die from "./Die";
 import { nanoid } from 'nanoid';
-import Confetti from 'react-confetti'; // instalirali
+import Confetti from 'react-confetti'; // install
 
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
-
-  // new state. It represents whether the user has won the game yet or not
   const [tenzies, setTenzies] = React.useState(false)
   
-  React.useEffect(() => {
+  React.useEffect(() => { 
     const allHeld = dice.every(die => die.isHeld)
-    // every - is it will look for a specific condition and if every item in that array returns a true for that condition then it will return the value true
-
-    // dal imaju isti value
     const firstValue = dice[0].value
     const allSameValue = dice.every(die => die.value === firstValue)
-    if (allHeld && allSameValue) { // ??? OBA UVJETA MORAJU BITI TOČNA? A AKO JE NPR. allHeld FALSE ONDA SE TO SVE ZANEMARI?
+    if (allHeld && allSameValue) { 
       setTenzies(true)
     }
-  }, [dice]) // to run every time the dice array changes in state
+  }, [dice]) 
 
 
   function generateNewDie() {
@@ -41,40 +36,32 @@ export default function App() {
   }
   
   function rollDice() {
-    // AKO JE TENZIES SUPROTAN OD TRENUTNE VRIJEDNOSTI ???
-    if (!tenzies) {
+    if (!tenzies) { 
       setDice(oldDice => oldDice.map(die => {
         return die.isHeld ? 
-          die : // sure that we just keep it as part of our array
+          die : 
           generateNewDie()
       })) 
     } else {
       setTenzies(false)
       setDice(allNewDice())
-    }
-      // if tenzis is true in other words if they've won the game then first I want to set tenzis to false
-      // because I don't want the game to still be won and then I can generate all new dice 
+    } 
   }
   
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
       return die.id === id ?
-        { ...die, isHeld: !die.isHeld } : // ??? ZA UPDATE DA AKO JE TRUE NEKA BUDE FALSE, A AKO JE FALSE NEKA STAVI TRUE
-        die // it's not the id that we clicked on or that we interacted with just keep the same object that we had before
+        { ...die, isHeld: !die.isHeld } : 
+        die 
     }))
   }
 
   const diceElements = dice.map(die => (
     <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id) } />
-    // mi smo napravili anonimnu funkciju. React će znati na koju kućicu smo klikli
-    /* jedan od načina je da moremo pass id to the die id={die.id} holdDice={holdDice} i onda da pozovemo holdDice na click
-    i pass the id that's passed as a prop here */
   ));
-
 
   return (
     <main>
-      {/* if tenzis are true then React will render Confetti */}
       {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
@@ -86,7 +73,6 @@ export default function App() {
       </div>
       <button className="roll-dice" onClick={rollDice}>
         {tenzies ? 'New game' : 'Roll'}
-        {/* if tenzies is true ispiši New game, ako je false roll*/}
       </button>
     </main>
   );
